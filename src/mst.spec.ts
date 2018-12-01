34068f3f758746dc9b37b36c77bf35ae1e174117
samuelalvin <samuel.alvin1994@gmail.com>
2018-12-01 20:31:40 +0700

    fix unit tests

--- src/mst.spec.ts
+++ src/mst.spec.ts
@@ -8,16 +8,51 @@ describe('mst unit tests', () => {
     });
     
     it('should fetch okay - try 1', async (done) =>{
+        const initialIsFetchSomethingDone = someRandomModel.isFetchSomethingDone;
+        const initialIsAnotherFetchDone = someRandomModel.isAnotherFetchDone;
+        const initialIsLastFetchDoneDone = someRandomModel.isLastFetchDone;
+
         await someRandomModel.fetchSomething();
-        expect(someRandomModel.isDoneFetching).toBeTruthy();
+
+        expect(initialIsFetchSomethingDone).toBeFalsy();
+        expect(initialIsAnotherFetchDone).toBeFalsy();
+        expect(initialIsLastFetchDoneDone).toBeFalsy();
+        expect(someRandomModel.isFetchSomethingDone).toBeTruthy();
+        expect(someRandomModel.isAnotherFetchDone).toBeTruthy();
+        expect(someRandomModel.isLastFetchDone).toBeTruthy();
         done();
     });
 
     it('should fetch okay - try 2', (done) =>{
+        const initialIsFetchSomethingDone = someRandomModel.isFetchSomethingDone;
+        const initialIsAnotherFetchDone = someRandomModel.isAnotherFetchDone;
+        const initialIsLastFetchDoneDone = someRandomModel.isLastFetchDone;
+
         someRandomModel.fetchSomething();
+
         setTimeout(() => {
-            expect(someRandomModel.isDoneFetching).toBeTruthy();
+            expect(initialIsFetchSomethingDone).toBeFalsy();
+            expect(initialIsAnotherFetchDone).toBeFalsy();
+            expect(initialIsLastFetchDoneDone).toBeFalsy();
+            expect(someRandomModel.isFetchSomethingDone).toBeTruthy();
+            expect(someRandomModel.isAnotherFetchDone).toBeTruthy();
+            expect(someRandomModel.isLastFetchDone).toBeTruthy();
             done();
         }, 200);
+    });
+
+    it('should fetch okay - try 3', async () =>{
+        const initialIsFetchSomethingDone = someRandomModel.isFetchSomethingDone;
+        const initialIsAnotherFetchDone = someRandomModel.isAnotherFetchDone;
+        const initialIsLastFetchDoneDone = someRandomModel.isLastFetchDone;
+
+        await someRandomModel.fetchSomething();
+
+        expect(initialIsFetchSomethingDone).toBeFalsy();
+        expect(initialIsAnotherFetchDone).toBeFalsy();
+        expect(initialIsLastFetchDoneDone).toBeFalsy();
+        expect(someRandomModel.isFetchSomethingDone).toBeTruthy();
+        expect(someRandomModel.isAnotherFetchDone).toBeTruthy();
+        expect(someRandomModel.isLastFetchDone).toBeTruthy();
     });
 });
--- src/mst.ts
+++ src/mst.ts
@@ -1,31 +1,29 @@
 import { types, flow, Instance } from 'mobx-state-tree';
 
 export const SomeRandomModel = types.model('someRandomModel', {
-    isFetching: types.optional(types.boolean, false),
-    isDoneFetching: types.optional(types.boolean, false)
+    isFetchSomethingDone: types.optional(types.boolean, false),
+    isAnotherFetchDone: types.optional(types.boolean, false),
+    isLastFetchDone: types.optional(types.boolean, false),
 }).actions(self => {
     const lastFetch = flow(function*() {
         yield Promise.resolve(4);
         yield Promise.resolve(4);
-        console.log('third fetch');
+        self.isLastFetchDone = true;
     });
 
     const anotherFetch = flow(function*() {
         yield Promise.resolve(1);
         yield Promise.resolve(1);
         yield lastFetch();
-        console.log('second fetch');
+        self.isAnotherFetchDone = true;
     });
 
     const fetchSomething = flow(function*() {
-        self.isFetching = true;
         yield anotherFetch();
         yield Promise.resolve(2);
         yield Promise.resolve(3);
         yield lastFetch();
-        console.log('first fetch');
-        self.isFetching = false;
-        self.isDoneFetching = true;
+        self.isFetchSomethingDone = true;
     });
 
     return {
